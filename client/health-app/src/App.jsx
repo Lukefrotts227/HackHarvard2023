d3db44a26f771fedfdf73146d0954182f48b5d6a
Ljf227 <ljfrotton@gmail.com>
2023-10-21 02:38:51 -0400

    adding the form

--- client/health-app/src/App.jsx
+++ client/health-app/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { InputForm } from "./components/inputform"; 
 
 function App() {
   
@@ -6,7 +6,15 @@ function App() {
   return (
     <>
       <main className="min-h-screen bg-gradient-to-t from-slate-200 to-slate-400">
+        <section>
+          <header className="text-4xl pt-4 text-extrabold"> 
+            <h1>Welcome to the app</h1>
+          </header>
+        </section>
 
+        <section className="grid place-self-center">  
+          <InputForm />
+        </section>
       </main>
     </>
   )
---
+++ client/health-app/src/components/inputForm.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'; 
+
+export const InputForm = () =>{
+
+    return(
+        <>
+            <main> 
+                <form>
+                
+                </form>
+
+            </main>
+        
+        </>
+        )
+}
