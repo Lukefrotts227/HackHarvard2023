62d2d53794130b20bd541bca3d0d5fc206dd3d46
Ljf227 <ljfrotton@gmail.com>
2023-10-21 07:50:43 -0400

    morning commit need to discuss more about the project before moving forward more

---
+++ client/health-app/src/components/longInputForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'; 
+
+export const LongInputForm = () =>{
+
+
+    return(
+        <main>
+            <form>
+                <textarea 
+                    placeholder="something should go here"/>
+            </form>
+        
+        </main>
+    )
+}
