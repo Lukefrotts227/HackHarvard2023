e30041403d666b9f65a641a2e8a6a36093536a40
Ljf227 <ljfrotton@gmail.com>
2023-10-21 15:11:05 -0400

    database setup

---
+++ server/db.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+
+import os
+
+from dotenv import load_dotenv
+
+
+
+def get_mongo_db_collection(collection): 
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+    load_dotenv(dotenv_path)
+
+    MONGODB_URI = os.getenv("MONGODB_URI")
+    try: 
+        client = MongoClient(MONGODB_URI)
+        db = client.get_database('cluster0')
+        collection = db[collection]
+        return collection
+    except Exception as e:
+        print("Error occured: ", e)
+        return None
+
+# from db import get_mongo_db_collection
