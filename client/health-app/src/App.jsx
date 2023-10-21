039d937cfc5cb0b62d2bba3df2d14ea22feaba30
Ljf227 <ljfrotton@gmail.com>
2023-10-21 02:33:34 -0400

    reset

--- client/health-app/src/App.jsx
+++ client/health-app/src/App.jsx
@@ -1,21 +1,15 @@
 
-import React, { useState, useEffect} from "react";
 
-import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
-import { InputForm } from "./components/inputform";
-
-export default function App() {
+function App() {
+  
 
   return (
     <>
-      <main className ='min-h-screen grid bg-gradient-to-t from-slate-400 to-slate-700'> 
-        <section> 
-          <header> 
-          </header>
-        </section>
+      <main className="min-h-screen bg-gradient-to-t from-slate-200 to-slate-400">
+
       </main>
     </>
   )
 }
 
+export default App
