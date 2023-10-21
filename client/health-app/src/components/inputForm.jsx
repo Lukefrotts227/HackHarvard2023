fc28add38c2352db1870fe8f377a22f5389dd21c
Ljf227 <ljfrotton@gmail.com>
2023-10-21 02:43:36 -0400

    styling form

--- client/health-app/src/components/inputForm.jsx
+++ client/health-app/src/components/inputForm.jsx
@@ -4,9 +4,12 @@ export const InputForm = () =>{
 
     return(
         <>
-            <main> 
-                <form>
-                
+            <main className="shadow-lg p-4 rounded-lg border-t-4 border-white"> 
+                <form className="flex flex-col gap-3 py-6 px-4"> 
+                    <input className="w-99 border border-gray-400" />
+                    <input className="w-99 border border-gray-400" />
+                    <input className="w-99 border border-gray-400"  />
+                    <button></button>
                 </form>
 
             </main>
