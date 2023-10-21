51732e7d5a49e87225cddf2d296ee1e61909241f
Ljf227 <ljfrotton@gmail.com>
2023-10-21 02:46:16 -0400

    styled form even more

--- client/health-app/src/App.jsx
+++ client/health-app/src/App.jsx
@@ -7,12 +7,12 @@ function App() {
     <>
       <main className="min-h-screen bg-gradient-to-t from-slate-200 to-slate-400">
         <section>
-          <header className="text-4xl pt-4 text-extrabold"> 
+          <header className="text-4xl pt-4 text-extrabold text-center"> 
             <h1>Welcome to the app</h1>
           </header>
         </section>
 
-        <section className="grid place-self-center">  
+        <section className="grid place-items-center pt-28">  
           <InputForm />
         </section>
       </main>
--- client/health-app/src/components/inputForm.jsx
+++ client/health-app/src/components/inputForm.jsx
@@ -4,7 +4,7 @@ export const InputForm = () =>{
 
     return(
         <>
-            <main className="shadow-lg p-4 rounded-lg border-t-4 border-white"> 
+            <main className="shadow-lg p-4 rounded-lg border-t-4 border-white bg-zinc-400"> 
                 <form className="flex flex-col gap-3 py-6 px-4"> 
                     <input className="w-99 border border-gray-400" />
                     <input className="w-99 border border-gray-400" />
