9722124d0a7ec7903e4b1f9618071aa617f0795b
Ljf227 <ljfrotton@gmail.com>
2023-10-21 20:14:24 -0400

    some flask functions

--- server/main.py
+++ server/main.py
@@ -46,6 +46,20 @@ def get_height(user):
         return jsonify({"height": user_data["height"]})
     return jsonify({"message": "User not found or height data missing"}, 404)
 
+@app.route('/users/getAge/<user>', methods=['GET'])
+def get_weight(user): 
+    user_data = database.find_one({"email": user})
+    if user_data and "age" in user_data:
+        return jsonify({"age": user_data["age"]})
+    return jsonify({"message": "User not found or age data missing"}, 404)
+
+@app.route('/users/getFamilyHistory/<user>', methods=['GET'])
+def get_height(user): 
+    user_data = database.find_one({"email": user})
+    if user_data and "familyHistory" in user_data:
+        return jsonify({"familyHistory": user_data["FamilyHistory"]})
+    return jsonify({"message": "User not found or height data missing"}, 404)
+
 @app.route('/users/setWeight/<user>', methods=['POST', 'GET'])
 def set_weight(user): 
     weight_data = request.get_json()
@@ -69,12 +83,38 @@ def set_height(user):
         return jsonify({"message": "Height updated"})
     return jsonify({"message": "User not found or height update failed"}, 404)
 
+
+@app.route('/users/setAge/<user>', methods=['POST', 'GET'])
+def set_weight(user): 
+    weight_data = request.get_json()
+    if not weight_data or "age" not in weight_data:
+        return jsonify({"message": "Invalid request data"}, 400)  # Return a 400 Bad Request response
+
+    result = database.update_one({"email": user}, {"$set": {"age": weight_data["age"]}})
+    if result.modified_count > 0:
+        return jsonify({"message": "Age updated"})
+    return jsonify({"message": "User not found or age update failed"}, 404)
+
+
+@app.route('/users/setFamilyHistory/<user>', methods=['POST', 'GET'])
+def set_height(user): 
+    height_data = request.get_json()
+    if not height_data or "familyHistory" not in height_data:
+        return jsonify({"message": "Invalid request data"}, 400)  # Return a 400 Bad Request response
+
+    result = database.update_one({"email": user}, {"$set": {"familyHistory": height_data["familyHistory"]}})
+    if result.modified_count > 0:
+        return jsonify({"message": "FamilyHistory updated"})
+    return jsonify({"message": "User not found or FamilyHistory update failed"}, 404)
+
 @app.route('/users/getName/<user>', methods=['GET', 'POST'])
 def get_name(user): 
     user_data = database.find_one({"email": user})
     if user_data and "name" in user_data:
         return jsonify({"name": user_data["name"]})
     return jsonify({"message": "User not found or name data missing"}, 404)
+
+
 
 
 if __name__ == "__main__":
